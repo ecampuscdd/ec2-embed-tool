a88435ea458e37e5c95d83b99ba30f6832e2db35
ecampuscdd <150379768+ecampuscdd@users.noreply.github.com>
2024-12-03 14:13:56 -0700

    Update main.js
    Updated with radio buttons and options for different words

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 let myForm = document.getElementById('mainForm');
-let textArea = document.getElementById('embedCode')
-let videoLink = document.getElementById('link');
+let textArea = document.getElementById('embedCode');
+let videoLink = document.getElementById('videoLink');
+let radioInfo = document.getElementsByName('transcriptAvailable');
+let transcriptLink = document.getElementById('transcriptLink');
 
 let videoID = '';
 let videoTimeEmbed = '';
 let videoTimeLink = '';
+let radioValue = '';
 
 let key = 'AIzaSyDv1p9Yae1_42wMx_jivmfiGhoBPzVKElk';
 let URL = 'https://youtube.googleapis.com/youtube/v3/videos';
@@ -12,13 +15,13 @@ let URL = 'https://youtube.googleapis.com/youtube/v3/videos';
 mainForm.onsubmit = function () {
   try {
     videoID = getIDFromLink(videoLink.value)[1];
-    console.log(getIDFromLink(videoLink.value)[2]);
+    console.log(getIDFromLink(videoLink.value)[0]);
     if (getIDFromLink(videoLink.value)[2]) videoTimeEmbed = `?start=${getIDFromLink(videoLink.value)[2]}`, videoTimeLink = `?t=${getIDFromLink(videoLink.value)[2]}`;
     else videoTimeEmbed = '', videoTimeLink = '';
     getEmbedCode(videoID);
   } catch(e) {
     $( "div.preview" ).html ( `<p>Please enter a valid YouTube link.</p>` );
-    textArea.value = 'Please enter a valid YouTube link.';
+	textArea.value = 'Please enter a valid YouTube link.';
   }
   return false;
 }
@@ -76,25 +79,38 @@ function getEmbedCode(id) {
  function getVideoDetails () {
   $.getJSON(URL, options, function(data) {
     try {
-      let title = data.items[0].snippet.title;
-      let duration = changeTimeFormat(data.items[0].contentDetails.duration);
-      let uploadDate = formatDate(data.items[0].snippet.publishedAt); // Get upload date and format it to en-US standard
-      let channelTitle = data.items[0].snippet.channelTitle; // Get channel title
+		let title = data.items[0].snippet.title;
+		let duration = changeTimeFormat(data.items[0].contentDetails.duration);
+		let uploadDate = formatDate(data.items[0].snippet.publishedAt); // Get upload date and format it to en-US standard
+		let channelTitle = data.items[0].snippet.channelTitle; // Get channel title
 
-      // Use the retrieved data to create the embed code
-      let embedCode = `
-        <p><iframe width="560" height="315" src="https://www.youtube.com/embed/${videoID}${videoTimeEmbed}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
-        <p>If the video doesn't appear, follow this direct link: <a class="inline_disabled" href="https://youtu.be/${videoID}${videoTimeLink}" target="_blank" rel="noopener">${title}</a> (${duration})</p>
-        <p>Use the direct link above to open the video in YouTube to display the video captions, expand the video, and navigate the video using the transcript.</p>
-        
-        <p>Video Uploaded: ${uploadDate} by ${channelTitle}</p>
-      `;
+		let transcriptOption = '';
+		console.log(transcriptLink.value);
+		for (i = 0; i < radioInfo.length; i++) {
+			if (radioInfo[i].checked){
+				radioValue = radioInfo[i].value;
+			}
+		}
+
+	  if(radioValue == "hasCaptions"){
+		transcriptOption = `</p>Use the direct link to open the video in YouTube to expand the video and display the video captions.</p>`;
+	  }else if(radioValue == "HaveLink"){
+		transcriptOption = `<p>Use the direct link to open the video in YouTube to display and expand the video. For a transcript, refer to the <a class="inline_disabled" href="${transcriptLink.value}" target="_blank" rel="noopener">${title} document</a>. </p>`;
+	  }else if(radioValue == "Ordered"){
+		transcriptOption = `<p>Use the direct link to open the video in YouTube to display and expand the video. For a transcript, refer to the ${title} document. </p>`;
+	  }else if(radioValue == "No"){
+		transcriptOption = `<p>Use the direct link to open the video in YouTube to display and expand the video.</p>
+		<p><strong>Note:</strong> Please contact your instructor if you require a detailed transcript of audio/video content.</p>`;
+	  }
+      // Use the retrieved data to create the embed code 
+      let embedCode = `<h3>Video: "${title}"</h3><p><iframe name="videoIframe" width="560" height="315" src="https://www.youtube.com/embed/${videoID}${videoTimeEmbed}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p><p>If the video doesn't appear, follow this direct link: <a class="inline_disabled" href="https://youtu.be/${videoID}${videoTimeLink}" target="_blank" rel="noopener">${title}</a> (${duration})</p>${transcriptOption}<p>Video uploaded: ${uploadDate} by ${channelTitle}.</p>`;
       textArea.value = embedCode;
-      $( "div.preview" ).html ( embedCode );
+      $( "div.preview" ).html (`<hr>`+embedCode );
     } catch(e) {
-      $( "div.preview" ).html ( `<p>Please enter a valid YouTube link.</p>` );
-      textArea.value = 'Please enter a valid YouTube link.'
+      $( "div.preview" ).html ( `<p>Cannot get embed code.</p>` );
+      textArea.value = 'Cannot get embed code.'
     }
   })
 }
+
 }
